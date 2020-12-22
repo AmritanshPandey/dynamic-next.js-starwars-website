19b3a49586f4986e50da7fc93d4f30870b069c25
unknown <you@example.com>
2020-12-22 21:33:04 +0530

    api integrated

--- starwars/pages/index.js
+++ starwars/pages/index.js
@@ -1,65 +1,42 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <h1 className="text-2xl text-center my-6 font-semibold">Starwars</h1>
+      <p className="mb-5">
+        Star Wars is an American epic space opera media franchise created by
+        George Lucas, which began with the eponymous 1977 film and quickly
+        became a worldwide pop-culture phenomenon. The franchise has been
+        expanded into various films and other media, including television
+        series, video games, novels, comic books, theme park attractions, and
+        themed areas, comprising an all-encompassing fictional universe. It
+        holds a Guinness World Records title for the "Most successful film
+        merchandising franchise." In 2020, its total value was estimated at
+        US$70 billion, and it is currently the fifth-highest-grossing media
+        franchise of all time.
+      </p>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <p className="mb-5">
+        The original film, retroactively subtitled Episode IV: A New Hope
+        (1977), was followed by the sequels Episode V: The Empire Strikes Back
+        (1980) and Episode VI: Return of the Jedi (1983), forming the original
+        Star Wars trilogy. Lucas later returned to filmmaking to direct a
+        prequel trilogy, consisting of Episode I: The Phantom Menace (1999),
+        Episode II: Attack of the Clones (2002), and Episode III: Revenge of the
+        Sith (2005). In 2012, Lucas sold his production company to Disney,
+        relinquishing his ownership of the franchise. The subsequently produced
+        sequel trilogy consists of Episode VII: The Force Awakens (2015),
+        Episode VIII: The Last Jedi (2017), and Episode IX: The Rise of
+        Skywalker (2019).
+      </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <p className="mb-5">
+        All nine films of the 'Skywalker saga' were nominated for Academy Awards
+        (with wins going to the first two released) and were commercially
+        successful. Together with the theatrical live action spin-off films
+        Rogue One (2016) and Solo (2018), the combined box office revenue of the
+        films equates to over US$10 billion, and it is currently the
+        second-highest-grossing film franchise of all time.
+      </p>
     </div>
-  )
+  );
 }
